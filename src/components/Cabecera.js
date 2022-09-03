ec0d569efd5d12220b666699c25c4dbe4895f35d
oribianchi <73916359+OriBianchi@users.noreply.github.com>
2022-09-03 03:25:09 -0300

    First commit

---
+++ src/components/Cabecera.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// El componente Cabera no tiene componentes hijos.
+// ESTADO: Cabecera no tiene estado.
+// MÉTODOS: Cabecera no tiene métodos.
+// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente
+// Maqueta de Cabecera:
+//    h1
+//    p > span     (el span mostrará la cantidad recibida por props)
+
+export default function Cabecera(props) {
+  
+  return (
+    <header>
+        <h1>
+          <p>
+            <span>
+              Artículos en tu carrito: {props.count}
+            </span>
+          </p>
+        </h1>
+    </header>
+  )
+}
---
+++ src/components/Item.js
@@ -0,0 +1,47 @@
+
+// El componente Item no tiene componentes hijos.
+// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
+// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
+// Maqueta de Item:
+//    h3
+//    p
+//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
+//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+
+import { useState } from 'react'
+
+export default function Item(props) {
+  let [tieneStock, noTieneStock] = useState(false);
+  let [cantStock, setCantStock] = useState(props.stock)
+
+  function agregarCarrito() {
+    let stock = cantStock;
+    props.incrementarCompras()
+    cantStock = setCantStock(cantStock - 1)
+    if (stock - 1 == 0) {
+      return noTieneStock(true);
+    } else {
+      return noTieneStock(false);
+    }
+  }
+
+  return (
+    <div className='producto'>
+      <h3>
+        {props.nombre}
+      </h3>
+      <p>
+        {props.descripcion}
+      </p>
+      <h5>
+        <span>
+          {cantStock}
+        </span>
+      </h5>
+      <button disabled={tieneStock} onClick={() => agregarCarrito()}>
+        {tieneStock ? "Sin stock" : "Añadir al carrito"}
+      </button>
+    </div>
+  )
+}
